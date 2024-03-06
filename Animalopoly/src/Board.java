bb20de16555f8a9f4fc045b4f343cc5580b6d8a6
Bhasvic-ZacCooper23 <145356910+Bhasvic-ZacCooper23@users.noreply.github.com>
2024-03-06 11:28:26 +0000

    Commented on my code

--- Animalopoly/src/Board.java
+++ Animalopoly/src/Board.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 
 public class Board
 {
-
+//initialise all object arrays
     ArrayList<Spaces> spaces = new ArrayList<>();
     ArrayList<Players> players = new ArrayList<>();
     Cards cards = new Cards();
+    //initialise the won bool
     Boolean won = false;
     public void intitialise()
     {
+        //add 26 spaces
         for (int i = 0; i < 26; i++)
         {
             spaces.add(new Spaces(i));
@@ -17,40 +19,54 @@ public class Board
         System.out.println("How many players?");
         Scanner scanner = new Scanner(System.in);
         int lim = Integer.parseInt(scanner.nextLine());
+        //create a player class per player asked for
         for (int i = 0; i < lim; i++)
         {
+            //add them to array
             players.add(new Players());
             System.out.println("What's player "+i+"'s name?");
+            //set their name
             players.get(i).setName(scanner.nextLine());
             System.out.println("What's player "+i+"'s symbol?");
+            //and their character
             players.get(i).setCharacter(scanner.nextLine());
         }
     }
     public void doATurn(int playerNum)
     {
-        if(!players.get(playerNum).getSkippedTurn())
+        //assign player to the current player's object
+        Players player = players.get(playerNum);
+        //check if their turn has been skipped
+        if(!player.getSkippedTurn())
         {
-            System.out.println("It's player " + playerNum + "'s turn!" + "\n type 'roll' to roll your dice!");
-            Die dice1 = new Die();
-            int roll1 = dice1.roll();
-            int roll2 = dice1.roll();
+            System.out.println("It's "+player.getName + "'s turn!" + "\n type 'roll' to roll your dice!");
+            //create and roll the dice
+            Die dice = new Die();
+            int roll1 = dice.roll();
+            int roll2 = dice.roll();
+            //check for a double
             if (roll1 == roll2) {
                 System.out.println("You rolled a " + (roll1 + roll2) + "!\nand it was a double!");
+                //get a card
                 System.out.println(cards.getCard(1));
             } else {
                 System.out.println("You rolled a " + (roll1 + roll2));
             }
-            players.get(playerNum).setPosition(players.get(playerNum).getPosition() + roll1 + roll2);
-            if(players.get(playerNum).getPosition()>26)
+            //move player what they rolled spaces
+            player.setPosition(player.getPosition() + roll1 + roll2);
+            //if they go past 26 move them back
+            if(player.getPosition()>26)
             {
-                players.get(playerNum).setPosition(players.get(playerNum).getPosition()-26);
+                player.setPosition(player.getPosition()-26);
             }
-            System.out.println(spaces.get(players.get(playerNum).getPosition()).getName()+"\n"+spaces.get(players.get(playerNum).getPosition()).getInfo());
+            //print out that spaces name and information
+            System.out.println(spaces.get(player.getPosition()).getName()+"\n"+spaces.get(player.getPosition()).getInfo());
         }
         else
         {
+            //if they've been skipped let them know and unskip them
             System.out.println("Your turn has been skipped");
-            players.get(playerNum).setSkippedTurn(false);
+            player.setSkippedTurn(false);
         }
     }
 }
--- Animalopoly/src/Main.java
+++ Animalopoly/src/Main.java
@@ -2,10 +2,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args)
     {
+        //create the board
         Board board = new Board();
+        //call the board method
         board.intitialise();
+        //until someone has won
         while(!board.won)
         {
+            //do turns for how many players there are
             for (int i = 0; i < board.players.size()-1; i++)
             {
                 board.doATurn(i);
