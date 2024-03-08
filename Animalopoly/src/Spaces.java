fc8d3d083dcb2f417a9b5941ac532903faa83377
Bhasvic-ZacCooper23 <145356910+Bhasvic-ZacCooper23@users.noreply.github.com>
2024-03-08 09:28:31 +0000

    Added Roberts code to main

--- Animalopoly/src/Spaces.java
+++ Animalopoly/src/Spaces.java
@@ -1,7 +1,42 @@
-public class Spaces
-{
-    public Spaces(int number)
-    {
+import org.w3c.dom.ls.LSOutput;
 
+public class Spaces {
+    private int num;
+    private String name;
+    private int owner;
+    private String fact;
+    private int upgradePrice;
+    private int upgradeLevel = 0;
+    SpacesInfo namer = new SpacesInfo();
+
+    public Spaces(int num) {
+        this.num = num;
+        name = namer.getSpaceName(this.num);
+        fact = namer.getSpaceFact(this.num);
+        upgradePrice = namer.setBasePrice();
     }
+    public void printName() {System.out.println(name);}
+    public String getName(){return name;}
+    public void printFact() {
+        System.out.println(fact);
+    }
+
+    public String getFact(){
+        return fact;
+    }
+
+    public void upgrade() {
+        if (upgradeLevel != 4) {
+            upgradePrice = namer.setUpgradePrice(upgradeLevel);
+            upgradeLevel = upgradeLevel + 1;
+        }
+    }
+
+    public int getUpgradePrice(){return upgradePrice;}
+
+    public int getLevel(){return upgradeLevel;}
+
+    public void setOwner(int ownNum){owner = ownNum;}
+
+    public int getOwner(){return owner;}
 }
