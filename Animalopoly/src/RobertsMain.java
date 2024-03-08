ffafff46497904f17052404d31e7a5285bf19978
BHASVIC-robertanderson23 <robert.anderson23@students.bhasvic.ac.uk>
2024-03-08 09:35:12 +0000

    Add my Main real quick (Working on merging)

---
+++ Animalopoly/src/RobertsMain.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class RobertsMain extends JFrame{
+    public RobertsMain() {}
+
+            public static void main(String[] args) {
+                ArrayList<Spaces> spaces = new ArrayList<Spaces>();
+
+                for (int i = 0; i < 27; i++) {
+                    spaces.add(new Spaces(i));
+                }
+
+                GUI mygui = new GUI(spaces);
+            }
+}
+
+
+
