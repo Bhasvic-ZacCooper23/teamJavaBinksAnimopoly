c5da6475af45b438964c00c1cc91d686d59ccac8
Bhasvic-ZacCooper23 <145356910+Bhasvic-ZacCooper23@users.noreply.github.com>
2024-03-08 09:23:52 +0000

    Updated Board.java

--- Animalopoly/src/Board.java
+++ Animalopoly/src/Board.java
@@ -40,57 +40,57 @@ public class Board {
         Scanner scanner = new Scanner(System.in);
         // assign player to the current player's object
         Players player = players.get(playerNum);
-        // check if their turn has been skipped
-        if (!player.getSkippedTurn()) {
-            System.out.println("It's " + player.getName() + "'s turn!" + "\n type 'roll' to roll your dice!");
-        //check if their turn has been skipped
-        if(!player.getSkippedTurn())
+        // check they haven't lost
+        if(player.getLost())
         {
-            System.out.println("It's "+player.getName + "'s turn!" + "\n type 'roll' to roll your dice!");
-            Die dice = new Die();
-            //create and roll the dice
-            int roll1 = dice.roll();
-            int roll2 = dice.roll();
-            // check for a double
-            if (roll1 == roll2) {
-                System.out.println("You rolled a " + (roll1 + roll2) + "!\nand it was a double!");
-                // get a card
-                System.out.println(cards.getCard(1));
-            } else {
-                System.out.println("You rolled a " + (roll1 + roll2));
-            }
-            // move player what they rolled spaces
-            player.setPosition(player.getPosition() + roll1 + roll2);
-            // if they go past 26 move them back
-            if (player.getPosition() > 26) {
-                player.setPosition(player.getPosition() - 26);
-            }
-            // assign space to the current space's object
-            Spaces space = spaces.get(player.getPosition());
-            // print out that spaces name and information
-            System.out.println(space.getName() + "\n" + space.getInfo());
-            // check if that space has been bought
-            if (space.getUpgradeLevel == 0)
+            // check if their turn has been skipped
+            if (!player.getSkippedTurn())
             {
-                System.out.println("This space has not been bought. \n Would you like to buy it?(Y/N)");
-                //if they want to buy it
-                if(scanner.nextLine().equals("Y"))
-                {
-                    if(player.getMoney()>=space.getUpgradePrice())
-                    {
-                        //take their money for the purchase
-                        player.setMoney(player.getMoney()-space.getUpgradePrice);
-                        space.setUpgradeLevel(1);
-                        space.setOwner(playerNum);
-                        //display the rent of the space
-                        System.out.println("This exhibit will cost "+space.getRent()+" to view");
+                System.out.println("It's " + player.getName() + "'s turn!" + "\nYou have £"+player.getMoney()+"\n type 'roll' to roll your dice!");
+                Die dice = new Die();
+                //create and roll the dice
+                int roll1 = dice.roll();
+                int roll2 = dice.roll();
+                // check for a double
+                if (roll1 == roll2) {
+                    System.out.println("You rolled a " + (roll1 + roll2) + "!\nand it was a double!");
+                    // get a card
+                    System.out.println(cards.getCard(1));
+                } else {
+                    System.out.println("You rolled a " + (roll1 + roll2));
+                }
+                // move player what they rolled spaces
+                player.setPosition(player.getPosition() + roll1 + roll2);
+                // if they go past 26 move them back
+                if (player.getPosition() > 26) {
+                    player.setPosition(player.getPosition() - 26);
+                }
+                // assign space to the current space's object
+                Spaces space = spaces.get(player.getPosition());
+                // print out that spaces name and information
+                System.out.println(space.getName() + "\n" + space.getInfo());
+                // check if that space has been bought
+                if (space.getUpgradeLevel == 0) {
+                    System.out.println("This space has not been bought. It costs: " + space.getUpgradePrice() + " \n Would you like to buy it?(Y/N)");
+                    //if they want to buy it
+                    if (scanner.nextLine().equals("Y")) {
+                        if (player.getMoney() >= space.getUpgradePrice()) {
+                            //take their money for the purchase
+                            player.setMoney(player.getMoney() - space.getUpgradePrice);
+                            space.setUpgradeLevel(1);
+                            space.setOwner(playerNum);
+                            //display the rent of the space
+                            System.out.println("This exhibit will cost " + space.getRent() + " to view");
+                        } else {
+                            System.out.println("Sorry you don't have enough money!");
+                        }
                     }
                 }
+            } else {
+                // if they've been skipped let them know and unskip them
+                System.out.println("Player " + playerNum + ", your turn has been skipped");
+                player.setSkippedTurn(false);
             }
-        } else {
-            // if they've been skipped let them know and unskip them
-            System.out.println("Player "+playerNum+", your turn has been skipped");
-            player.setSkippedTurn(false);
         }
     }
 }
