bc3cf374da192862d8f7fe4af108f1f632b4eb71
Bhasvic-ZacCooper23 <145356910+Bhasvic-ZacCooper23@users.noreply.github.com>
2024-03-21 08:43:06 +0000

    Update Die.java

--- Animalopoly/src/Die.java
+++ Animalopoly/src/Die.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Die {
     public Random random = new Random();
 
-    public int roll() {
+    public int roll()
+    {
         return random.nextInt(6)+1; // create random number between 1 and 6
-
     }
 }
